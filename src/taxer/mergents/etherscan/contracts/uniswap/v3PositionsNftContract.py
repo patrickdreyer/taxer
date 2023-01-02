97842fb641e7b521107ed2edc4771f19eafad0ab
Patrick Dreyer <Patrick@Dreyer.name>
2023-01-02 15:28:02 +0100

    Fix: Liquidity pool fees way too high

--- src/taxer/mergents/etherscan/contracts/uniswap/v3PositionsNftContract.py
+++ src/taxer/mergents/etherscan/contracts/uniswap/v3PositionsNftContract.py
@@ -39,13 +39,13 @@ class V3PositionsNftContract(Contract):
                 output = Ether.decodeContractEventData(self.__web3Contract, 'IncreaseLiquidity', collectLog['topics'], collectLog['data'])
                 (amount0, amount1) = self.__pools.increase(poolId, output['liquidity'], output['amount0'], output['amount1'])
                 if transaction['dateTime'].year == year:
-                    fee = Ether.amountFromTransaction(transaction)
+                    fee = Ether.feeFromTransaction(transaction)
                     yield AddLiquidity(id, transaction['dateTime'], transaction['hash'], amount0, amount1, fee, poolId, V3PositionsNftContract.__publicNameTag)
             elif name == 'decreaseliquidity':
                 poolId = int(args['params'][0])
                 (amount0, amount1) = self.__pools.decrease(poolId, args['params'][1], args['params'][2], args['params'][3])
                 if transaction['dateTime'].year == year:
-                    fee = Ether.amountFromTransaction(transaction)
+                    fee = Ether.feeFromTransaction(transaction)
                     yield RemoveLiquidity(id, transaction['dateTime'], transaction['hash'], amount0, amount1, fee, poolId, V3PositionsNftContract.__publicNameTag)
             elif name == 'collect':
                 if transaction['dateTime'].year == year:
@@ -53,7 +53,7 @@ class V3PositionsNftContract(Contract):
                     collectLog = self.__etherscanApi.getFirstLog(transaction['blockNumber'], address = V3PositionsNftContract.__address, topic0 = V3PositionsNftContract.__collectTopic, topic1 = Ether.toTopic(poolId))
                     output = Ether.decodeContractEventData(self.__web3Contract, 'Collect', collectLog['topics'], collectLog['data'])
                     (amount0, amount1) = self.__pools.collect(poolId, output['amount0'], output['amount1'])
-                    fee = Ether.amountFromTransaction(transaction)
+                    fee = Ether.feeFromTransaction(transaction)
                     yield ClaimLiquidityFees(id, transaction['dateTime'], transaction['hash'], amount0, amount1, fee, poolId, V3PositionsNftContract.__publicNameTag)
             else:
                 raise KeyError(f"Unknown contract multicall function; contract='{V3PositionsNftContract.__publicNameTag}', functionName='multicall.{name}'")
@@ -72,7 +72,7 @@ class V3PositionsNftContract(Contract):
         liquidity = output['liquidity']
         amount0 = contract0.amount(output['amount0'])
         amount1 = contract1.amount(output['amount1'])
-        fee = Ether.amountFromTransaction(transaction)
+        fee = Ether.feeFromTransaction(transaction)
         self.__pools.create(poolId, liquidity, contract0, contract1, amount0, amount1)
         if transaction['dateTime'].year == year:
             yield CreateLiquidityPool(id, transaction['dateTime'], transaction['hash'], amount0, amount1, fee, poolId, V3PositionsNftContract.__publicNameTag)
