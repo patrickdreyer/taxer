5b4bca2cad5b74a6fd7014f8866d3862b9397ef6
Patrick Dreyer <Patrick@Dreyer.name>
2023-01-08 23:37:29 +0100

    Change: Make Coinbase mergent configurable

--- src/taxer/mergents/coinbase/coinbaseMergent.py
+++ src/taxer/mergents/coinbase/coinbaseMergent.py
@@ -6,6 +6,7 @@ from .coinbaseFileReader import CoinbaseFileReader
 class CoinbaseMergent(Mergent):
     def __init__(self, container:Container, config):
         self.__container = container
+        self.__config = config
 
     def createReaders(self):
-        yield CoinbaseFileReader(self.__container['config']['input'])
+        yield CoinbaseFileReader(self.__config['id'], self.__container['config']['input'], self.__config['fileNamePattern'])
