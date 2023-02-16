75e4f2a7710c8a70895e5b6a555c5acf6120f32c
Patrick Dreyer <Patrick@Dreyer.name>
2023-02-16 01:26:30 +0100

    Add: Export interests

--- src/taxer/accounting/banana/bananaStrategy.py
+++ src/taxer/accounting/banana/bananaStrategy.py
@@ -5,9 +5,14 @@ from .bananaCurrency import BananaCurrency
 class BananaStrategy:
     def __init__(self, container:Container):
         self._container = container
+        self.__interests = []
 
     def initialize(self): pass
     def finalize(self): yield from []
+
+    @property
+    def interests(self):
+        return self.__interests
 
     def doesTransform(self, transaction): pass
     def transform(self, transaction): pass
@@ -18,3 +23,8 @@ class BananaStrategy:
     def _book(self, transaction, args):
         pre = [transaction.dateTime.date().strftime('%d.%m.%Y'), transaction.id]
         return (transaction.dateTime.date(), pre + args)
+
+    def _interest(self, transaction, args):
+        pre = [transaction.dateTime.date().strftime('%d.%m.%Y'), transaction.id]
+        entry = (transaction.dateTime.date(), pre + args)
+        self.__interests.append(entry)
