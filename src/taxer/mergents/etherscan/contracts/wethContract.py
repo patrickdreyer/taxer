4f6aab6f5f6620e4923b8e38cbd1ef38d9910d82
Patrick Dreyer <Patrick@Dreyer.name>
2023-11-15 19:18:46 +0100

    Remove: Dead code

--- src/taxer/mergents/etherscan/contracts/wethContract.py
+++ src/taxer/mergents/etherscan/contracts/wethContract.py
@@ -10,7 +10,6 @@ class WethContract(Contract):
 
     def __init__(self, contracts, accounts:list[str], etherscanApi):
         super().__init__('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', None, accounts)
-        self.__contract = etherscanApi.getContract(self.address)
 
     def processTransaction(self, address, id, year, transaction, erc20Transaction):
         raise NotImplementedError(f"Not implemented contact; id={WethContract.__id}")
